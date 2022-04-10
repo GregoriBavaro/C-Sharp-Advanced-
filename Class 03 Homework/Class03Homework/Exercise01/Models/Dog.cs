f78425d5e704a8c56a52347f4b6f2bc1d9177aa5
Gregori <greg.gego@gmail.com>
2022-04-10 19:32:04 +0200

    Homework

---
+++ Class 03 Homework/Class03Homework/Exercise01/Models/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Exercise01.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} is barking, BARK BARK BARK!");
+        }
+
+        public static bool Validate(Dog validate)
+        {
+            return validate.Id > 0 && validate.Name.Length >= 2 && validate.Color != string.Empty;
+        }
+
+        public Dog(int id, string name, string color)
+        {
+            Id = id;
+            Name = name;
+            Color = color;
+        }
+    }
+}
---
+++ Class 03 Homework/Class03Homework/Exercise01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Exercise01.Models;
+
+namespace Exercise01
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dog dog1 = new Dog(1, "Cooper", "Brown");
+            Dog dog2 = new Dog(2, "Max", "Brown");
+            Dog dog3 = new Dog(3, "Oscar", "Golden");
+            Dog dog4 = new Dog(4, "Ruby", "White");
+            Dog dog5 = new Dog(5, "Walter", "White");
+
+            Console.WriteLine("Checking if the class properties are all Valid");
+
+            Console.WriteLine(Dog.Validate(dog1));
+            Console.WriteLine(Dog.Validate(dog2));
+            Console.WriteLine(Dog.Validate(dog3));
+            Console.WriteLine(Dog.Validate(dog4));
+            Console.WriteLine(Dog.Validate(dog5));
+
+            DogShelter.Dogs.Add(dog1);
+            DogShelter.Dogs.Add(dog2);
+            DogShelter.Dogs.Add(dog3);
+            DogShelter.Dogs.Add(dog4);
+            DogShelter.Dogs.Add(dog5);
+
+            DogShelter.PrintAll();
+
+            dog1.Bark();
+        }
+    }
+}
